0fb72689ac2f14af6630deae83585ab4134fcb2a
ZenRay <jake2011ren@gmail.com>
2021-01-12 14:45:19 +0800

    test($cell): Add Cell Tests
    添加 cell 测试:1.属性值测试，2. 有效性检查测试

---
+++ tests/test_bords/test_cell.py
@@ -0,0 +1,36 @@
+#coding:utf8
+import unittest
+import pytest
+from unittest import mock
+
+
+from source.board import Cell
+
+class CellTest(unittest.TestCase):
+    """Test Board Cell"""
+    def setUp(self) -> None:
+        self.cell = Cell("A", 1, 9)
+
+    def test_property(self):
+        """Test Cell Property"""
+        self.assertEqual(self.cell.row, "A")
+        self.assertEqual(self.cell.column, 1)
+        self.assertEqual(self.cell.value, 9)
+
+    def test_repr(self):
+        cell = self.cell
+        value = f"<Cell({cell.row}, {cell.column}) {cell.value} at {hex(id(cell))}/>"
+        self.assertEqual(repr(self.cell), value)
+
+
+    def test_print(self):
+        self.assertEqual(str(self.cell), "| 9 |")
+
+
+    def test_validcheck(self):
+        """Cell Value Valid Status"""
+        obj = mock.Mock()
+        obj.valid.side_effect = lambda cell, options: cell.value not in options
+
+        self.assertTrue(obj.valid(self.cell, [0, 1, 3]))
+        self.assertFalse(obj.valid(self.cell, [1, 9, 2]))
