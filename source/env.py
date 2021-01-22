fda0d16cd74b02d27e47d7597786de4a9e6bd378
ZenRay <jake2011ren@gmail.com>
2021-01-22 10:06:21 +0800

    feat($board $env): Add Board Box
    添加 Board 作为环境控制项，是数独的边界

--- source/env.py
+++ source/env.py
@@ -1,5 +1,8 @@
 #coding:utf8
 
+# row start charater
+ROW_START = ord("A")
+
 
 class Cell:
     def __init__(self, row, column, value=None):
@@ -38,5 +41,67 @@ class Cell:
         """
         return value not in filter
 
+
 class Board:
-    pass
+    def __init__(self, rows, cols, grid=3):
+        """Board Enviroment
+        
+        """
+        self.rows = rows
+        self.cols = cols
+        self.boxes = []
+        self.grid = grid
+
+    def _create(self, values=None):
+        """Create Board Box
+
+        If grid values exist, initialize the cell value, otherwise setup None
+        """
+        assert values is None or self.rows * self.cols == len(values), \
+            "Grid length isn't equal with total of the box cell"
+
+        for row in range(self.rows):
+            rows = []
+            for col in range(self.cols):
+                # get cell value
+                if values is not None:
+                    value = values[0]
+                    values = values[1:]
+                else:
+                    value = None
+                
+                rows.append(Cell(chr(row + ROW_START), col, value))
+                
+            # put rows cell into box
+            self.boxes.append(rows)
+
+    
+    def __repr__(self):
+        """Display Box
+
+        Format string of the box, like this:
+            . . 3 |. 2 . |6 . . 
+            9 . . |3 . 5 |. . 1 
+            . . 1 |8 . 6 |4 . . 
+            ------+------+------
+            . . 8 |1 . 2 |9 . . 
+            7 . . |. . . |. . 8 
+            . . 6 |7 . 8 |2 . . 
+            ------+------+------
+            . . 2 |6 . 9 |5 . . 
+            8 . . |2 . 3 |. . 9 
+            . . 5 |. 1 . |3 . .
+        """
+        fmt = []
+        for i, row in enumerate(self.boxes, 1):
+            line = ""
+            for index, cell in enumerate(row, 1):
+                if index != len(row) and index % 3 == 0:
+                    line += f"{cell.value} " + "|"
+                else:
+                    line += f"{cell.value} "
+            fmt.append(line)
+
+            if i % 3 == 0 and i != len(self.boxes):
+                fmt.append("+".join(["-" * self.grid * 2] * self.grid))
+        return "\n".join(fmt)
